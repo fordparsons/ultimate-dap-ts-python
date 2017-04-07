d13c204291bb649f957a80b1d86e7ba4f62e4c4b
William Parsons <wiparsons@Williams-MacBook-Pro.local>
2017-04-07 12:38:30 -0500

    add helper function to pull sections and parameters

--- dhd12_bashscript.py
+++ dhd12_bashscript.py
@@ -7,9 +7,25 @@ print "pulling variables from the configuration file"
 #perhaps "did you move configuration file?" "if so, what's the new path?"
 
 #engage the config parser to pull in our configuration file
+#need to adjust path to config file to default
 Config = ConfigParser.ConfigParser()
-Config
-Config.read("/opt/duoauthproxy/conf")
+Config.read("authproxy.cfg")
 
 #getting verbose here...but list out the sections found
-Config.sections()
+#print Config.sections()
+
+#pull in individual config parameters
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+print ConfigSectionMap("ad_client")['host']
