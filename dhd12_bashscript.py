79696b784060fb30618f64abdb5e0f183128c9a0
Ford Parsons <wiparsons@Williams-MacBook-Pro.local>
2017-05-13 17:21:35 -0700

    build AD test function and begin test-to-run-logic

--- dhd12_bashscript.py
+++ dhd12_bashscript.py
@@ -14,7 +14,9 @@ print "pulling variables from authproxy.cfg"
 Config = ConfigParser.ConfigParser()
 Config.read("authproxy.cfg")
 
-#pull in individual config parameters
+# Define some functions for tests
+
+# Define a function to pull in configuration attributes
 def ConfigSectionMap(section):
     dict1 = {}
     options = Config.options(section)
@@ -28,34 +30,41 @@ def ConfigSectionMap(section):
             dict1[option] = None
     return dict1
 
+# Define a function to test AD host connectivity
+def DuoAcDirTest(ad_host):
+    try:
+        ad_ping_response = os.system("ping -c 1 " + ad_host)
+        if ad_ping_response == 0:
+            ad_reachable = True
+        else:
+            ad_reachable = False
+    except:
+        print 'DuoAcDirTest Failed!'
+    return ad_reachable
+
 # define host locations
 # This section needs to be logic'd to only search attributes included
 # Perhaps we need to create a list of included variables for later tests' logic
 localhost = "127.0.0.1"
-ad_host = ConfigSectionMap("ad_client")['host']
-radius_host = ConfigSectionMap("radius_server_auto")['radius_ip_1']
+if Config.has_section("ad_client") == True:
+    if DuoAcDirTest(ConfigSectionMap("ad_client")['host']) == True:
+        print 'Active Directory Reachable'
+    else:
+        print 'Active Directory NOT Reachable'
 
-#test network connectivity to ad host
-#This section needs to be silenced somehow
-ad_ping_response = os.system("ping -c 1 " + ad_host)
 
-#and then check the response...
-if ad_ping_response == 0:
-  print "Active Directory is Reachable!"
-else:
-  print "Where's that Active Directory?"
 
 #time to test a port
-ldap_port = 443
+#ldap_port = 443
 
-print "are we listening for 1812?"
-socket_test = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_test.settimeout(2)                                      #2 Second Timeout
-result = socket_test.connect_ex((ad_host,ldap_port))
-if result == 0:
-  print 'port OPEN'
-else:
-  print 'port CLOSED, connect_ex returned: '+str(result)
+#print "are we listening for 1812?"
+#socket_test = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#socket_test.settimeout(2)                                      #2 Second Timeout
+#result = socket_test.connect_ex((ad_host,ldap_port))
+#if result == 0:
+#  print 'port OPEN'
+#else:
+ # print 'port CLOSED, connect_ex returned: '+str(result)
 
   #testing the features of github desktop
   #further testing y'all
